5a4f29f8b37848a19609d67a062843df5402d9a0
Nazar <fedunnazar20122001@gmail.com>
2023-04-12 16:12:46 +0300

    update increase and decrease counters to the events

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,28 @@ function App() {
   const [counter, setCounter] = useState<number>(0);
   const counter1 = useState<number>(0);
 
-  if (counter < 0) {
-    setCounter(0);
+  const increaseCounter = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const text = e.currentTarget.innerHTML;
+    if (!text.includes("counter1")) {
+      setCounter(counter + 1);
+    }
+    else {
+      counter1[1](counter1[0] + 1);
+    }
   }
-  if (counter1[0] < 0) {
-    counter1[1](0);
+  const decreaseCounter = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const text = e.currentTarget.innerHTML;
+
+    if (!text.includes("counter1")) {
+      if (counter != 0) {
+        setCounter(counter - 1);
+      }
+    }
+    else {
+      if (counter1[0] != 0) {
+        counter1[1](counter1[0] - 1);
+      }
+    }
   }
 
   return (
@@ -26,13 +43,14 @@ function App() {
       </Counter> */}
       <h4>Counter</h4>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>counter + 1</button>
-      <button onClick={() => setCounter(counter - 1)}>counter - 1</button>
+      {/* <button onClick={() => setCounter(counter + 1)}>counter + 1</button> */}
+      <button onClick={increaseCounter}>counter + 1</button>
+      <button onClick={decreaseCounter}>counter - 1</button>
 
       <h4>Counter1</h4>
       <div>{counter1[0]}</div>
-      <button onClick={() => counter1[1](counter1[0] + 1)}>counter1 + 1</button>
-      <button onClick={() => counter1[1](counter1[0] - 1)}>counter1 - 1</button>
+      <button onClick={increaseCounter}>counter1 + 1</button>
+      <button onClick={decreaseCounter}>counter1 - 1</button>
     </div>
   );
 }
