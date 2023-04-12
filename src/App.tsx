e009318009a3f0f32de0c392ec9c0548b36c8ab5
Nazar <fedunnazar20122001@gmail.com>
2023-04-12 15:48:54 +0300

    add useState using indexes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ function App() {
   // hooks
   // render props
   const [counter, setCounter] = useState<number>(0);
+  const counter1 = useState<number>(0);
 
   if (counter < 0) {
     setCounter(0);
+  }
+  if (counter1[0] < 0) {
+    counter1[1](0);
   }
 
   return (
@@ -20,9 +24,15 @@ function App() {
           <button onClick={() => setCount(count + 1)}>+</button>
         </div>)}
       </Counter> */}
+      <h4>Counter</h4>
       <div>{counter}</div>
       <button onClick={() => setCounter(counter + 1)}>counter + 1</button>
       <button onClick={() => setCounter(counter - 1)}>counter - 1</button>
+
+      <h4>Counter1</h4>
+      <div>{counter1[0]}</div>
+      <button onClick={() => counter1[1](counter1[0] + 1)}>counter1 + 1</button>
+      <button onClick={() => counter1[1](counter1[0] - 1)}>counter1 - 1</button>
     </div>
   );
 }
