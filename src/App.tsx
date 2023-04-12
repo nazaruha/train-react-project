905a0efa36f64cfba0bcb0457bad7805ae5eded0
Nazar <fedunnazar20122001@gmail.com>
2023-04-12 15:32:56 +0300

    make counter calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from './TextField';
 import { Counter } from './Counter';
 
@@ -6,15 +6,23 @@ function App() {
   // props
   // hooks
   // render props
+  const [counter, setCounter] = useState<number>(0);
+
+  if (counter < 0) {
+    setCounter(0);
+  }
 
   return (
     <div>
       {/* <TextField text='hello' person={{firstName: 'Nazarii', lastName: 'Fedun'}} obj={{f1: ''}}/> */}
-      <Counter>
+      {/* <Counter>
         {(count, setCount) => (<div>{count}
           <button onClick={() => setCount(count + 1)}>+</button>
         </div>)}
-      </Counter>
+      </Counter> */}
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>counter + 1</button>
+      <button onClick={() => setCounter(counter - 1)}>counter - 1</button>
     </div>
   );
 }
