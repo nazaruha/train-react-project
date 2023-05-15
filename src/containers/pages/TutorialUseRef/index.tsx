f4cb0d4d11e3581b8eec8bcdfe77c6727dcc56aa
Nazar <fedunnazar20122001@gmail.com>
2023-05-15 13:55:39 +0300

    accomplish useRef train

--- src/containers/pages/TutorialUseRef/index.tsx
+++ src/containers/pages/TutorialUseRef/index.tsx
@@ -8,15 +8,63 @@ const TutorialUseRef: React.FC = () => {
     // work with main div
 
     const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const submitButtonRef = useRef<HTMLButtonElement>(null);
+
+    const checkBoxRef = useRef<HTMLInputElement>(null);
+    const agreeButtonRef = useRef<HTMLButtonElement>(null);
+    const disagreeButtonRef = useRef<HTMLButtonElement>(null);
 
     const SubmitData = (e: any) => {
         e.preventDefault();
-        console.log(emailRef.current?.value);
+        alert(`Data: ${emailRef.current?.value}`);
+        console.log(emailRef.current?.value + ' ' + passwordRef.current?.value);
     }
 
     const EmailInputFocus = () => {
         console.log(emailRef.current?.focus())
         console.log(emailRef.current?.getBoundingClientRect())
+    }
+
+    const EmailChangeFon = () => {
+        if (emailRef.current) {
+            if (emailRef.current.style.background === "red") {
+                emailRef.current.style.background = "none"
+                return;
+            }
+            emailRef.current.style.background = "red";
+        }
+    }
+
+    const EmailKeyUpHandler = (e: any) => {
+        if (e.key === "Enter") {
+            passwordRef.current ? passwordRef.current.focus() : console.log("input is null");
+        }
+    }
+
+    const PasswordKeyUpHandler = (e: any) => {
+        if (e.key === "Enter") {
+            submitButtonRef.current ? submitButtonRef.current.click() : console.log("button is null");
+        }
+    }
+
+    const onCheckBoxClick = (e: any) => {
+        // e.preventDefault();
+        if (checkBoxRef.current) {
+            const isChecked: boolean = checkBoxRef.current.checked;
+            switch (isChecked) {
+                case true: {
+                    agreeButtonRef.current ? agreeButtonRef.current.disabled = true : console.log("button is null");
+                    disagreeButtonRef.current ? disagreeButtonRef.current.disabled = false : console.log("button is null");
+                    return;
+                }
+                case false: {
+                    disagreeButtonRef.current ? disagreeButtonRef.current.disabled = true : console.log("button is null");
+                    agreeButtonRef.current ? agreeButtonRef.current.disabled = false : console.log("button is null");
+                    return;
+                }
+            }
+        }
     }
 
     // work with second div
@@ -29,12 +77,17 @@ const TutorialUseRef: React.FC = () => {
 
             <div className="container-fluid mt-5 mb-3 d-flex justify-content-center" id="main-div">
                 <form style={{ width: '50%' }} onSubmit={SubmitData}>
-                    <div className="mb-3">
+                    <div className="mb-1">
                         <label htmlFor="email" className="form-label">Email Address</label>
-                        <input type="text" className="form-control" id="email" ref={emailRef} />
+                        <input type="text" className="form-control" id="email" ref={emailRef} onKeyUp={EmailKeyUpHandler} />
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" ref={passwordRef} onKeyUp={PasswordKeyUpHandler} />
+                    </div>
+                    <button type="submit" className="btn btn-primary" ref={submitButtonRef}>Submit</button>
                     <button className='btn btn-warning ms-1' onClick={EmailInputFocus}>Focus</button>
+                    <button className='btn btn-secondary ms-1' onClick={EmailChangeFon}>Change Fon</button>
                 </form>
             </div>
 
@@ -53,6 +106,38 @@ const TutorialUseRef: React.FC = () => {
                 >
                     Reset counter
                 </button>
+            </div>
+
+            <h1 className='text-center mt-5'><u>useRef</u> Check Box</h1>
+
+            <div className="container-fluid mt-5 d-flex justify-content-center" id='third-div'>
+                <div className="row">
+                    <div className="col-12">
+                        <input ref={checkBoxRef} className="form-check-input" type="checkbox" value="" id="flexCheckDefault" onClick={onCheckBoxClick} />
+                        <label className="form-check-label" htmlFor="flexCheckDefault" onClick={onCheckBoxClick}>
+                            Default checkbox
+                        </label>
+                    </div>
+                    <div className="col-6">
+                        <button className="btn btn-success w-75" ref={agreeButtonRef} onClick={(e) => {
+                            if (checkBoxRef.current) {
+                                checkBoxRef.current.checked = true;
+                                e.currentTarget.disabled = true;
+                                disagreeButtonRef.current ? disagreeButtonRef.current.disabled = false : console.log("button is null");
+                            }
+                        }}>AgreeRef</button>
+                    </div>
+                    <div className="col-6">
+                        <button className="btn btn-danger w-75" ref={disagreeButtonRef} onClick={(e) => {
+                            if (checkBoxRef.current) {
+                                checkBoxRef.current.checked = false;
+                                e.currentTarget.disabled = false;
+                                agreeButtonRef.current ? agreeButtonRef.current.disabled = false : console.log("button is null");
+                            }
+                        }}>DisagreeRef</button>
+                    </div>
+                </div>
+
             </div>
         </>
     )
